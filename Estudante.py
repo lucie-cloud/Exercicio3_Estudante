5b6ef17ef6505855495518ed3841b6e4b44399f5
lucie-cloud <luhaya@yahoo.com.br>
2023-12-18 10:34:01 -0300

    Commit

---
+++ Estudante.py
@@ -0,0 +1,27 @@
+# classe de teste para Estudante
+
+class Estudante(object):
+
+    def __init__(self, nome, notas):
+        self.nomes = nome
+        self.notas = notas
+
+        def setnomes(self, nome):
+            self.nomes - nome
+
+            def getnomes(self):
+                return set.nome
+
+            def setnotas(self, notas):
+                self.notas = notas
+
+                def getnotas(self):
+                    return set.notas
+
+    # Exibir dados do Objeto
+
+    def __float__(self):
+        return (
+
+
+        )
---
+++ testeEstudante.py
@@ -0,0 +1,19 @@
+# Teste classe Estudante - média bimestral
+
+from estudante import Estudante
+
+
+# Entrada
+
+n1 = float(input('Infome nota 1: '))
+n2 = float(input('Informe nota 2: '))
+
+
+# Processamento
+
+média bimestral = (n1 + n2)/2
+
+
+# Exibindo a consulta média bimestral
+print('\n\t\t\t\ --Consulta média bimestral --\n')
+print
